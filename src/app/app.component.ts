90224c349118ae4db74fa5a5d01bfe6333fef272
Valery <valery.chumpitaz@vallegrnade.edu.pe>
2025-09-18 16:33:41 -0500

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { DetalleUsuarioComponent } from './componentes/detalle-usuario/detalle-usuario.component';
 import { ListaUsuariosComponent } from './componentes/lista-usuarios/lista-usuarios.component';
-import { Usuario } from './componentes/model/usuario.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule,
-    HttpClientModule,         
-    ListaUsuariosComponent,
-    DetalleUsuarioComponent
+    HttpClientModule,
+    ListaUsuariosComponent
   ],
   template: `
     <h1>Gestión de Usuarios</h1>
-
-   s <app-lista-usuarios>
-    </app-lista-usuarios>
-
-    <app-detalle-usuario
-      *ngIf="usuarioSeleccionado"
-      [usuario]="usuarioSeleccionado">
-    </app-detalle-usuario>
+    <app-lista-usuarios></app-lista-usuarios>
   `,
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  usuarioSeleccionado: Usuario | null = null;
-
-  onUsuarioSeleccionado(usuario: Usuario) {
-    this.usuarioSeleccionado = usuario;
-  }
-}
+export class AppComponent {}
--- src/app/componentes/lista-usuarios/lista-usuarios.component.ts
+++ src/app/componentes/lista-usuarios/lista-usuarios.component.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { UsuarioService } from '../../servicios/usuario.service';
 import { Usuario } from '../model/usuario.model';
 
-
 @Component({
   selector: 'app-lista-usuarios',
   standalone: true,
@@ -15,9 +14,7 @@ export class ListaUsuariosComponent implements OnInit {
   usuarios: Usuario[] = [];
   usuarioSeleccionado?: Usuario;
 
-
   constructor(private usuarioService: UsuarioService) {}
-
 
   ngOnInit() {
     this.usuarioService.getUsuarios().subscribe({
@@ -26,16 +23,11 @@ export class ListaUsuariosComponent implements OnInit {
     });
   }
 
-
   verDetalle(usuario: Usuario) {
     this.usuarioSeleccionado = usuario; // abre el modal
   }
-
 
   cerrarModal() {
     this.usuarioSeleccionado = undefined; // cierra el modal
   }
 }
-
-
-
